bee596fd0ff8840bb2d121753f321c990ddfe309
Scott Turnbull <streamweaver@gmail.com>
2017-08-24 09:25:05 -0400

    added default dataservice injection

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-sandbox',
@@ -10,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SandboxComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dataService: DataService) { }
 
   ngOnInit() {
   }
