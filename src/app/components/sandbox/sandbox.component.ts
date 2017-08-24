6030475a84b3a7ec5e559db622d235511fa6b563
Scott Turnbull <streamweaver@gmail.com>
2017-08-24 10:07:08 -0400

    http service setup for rest of section six

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -7,11 +7,24 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./sandbox.component.css'],
   template: `
     <h1>Hello Sandbox!</h1>
+    <div *ngFor="let user of users" class="container">
+      <div class="well">
+        <ul class="list-group">
+          <li class="list-group-item">{{ user.name }}</li>
+          <li class="list-group-item">{{ user.email }}</li>
+          <li class="list-group-item">{{ user.phone }}</li>
+        </ul>
+      </div>
+    </div>
   `
 })
 export class SandboxComponent implements OnInit {
-
-  constructor(public dataService: DataService) { }
+  users: any[];
+  constructor(public dataService: DataService) {
+    this.dataService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
+  userServiceUrl = 'http://jsonplaceholder.typicode.com/users';
+  constructor(public http: Http) { }
 
-  constructor() { }
-
+  getUsers() {
+    return this.http.get(this.userServiceUrl)
+      .map(res => res.json());
+  }
 }
